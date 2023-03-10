31f93fc778c1eb5aff7ddd627a3f87303d741993
Stephane ASSOCLE <mensste30@gmail.com>
2023-03-09 16:10:06 -0800

    2. How to make a script dynamic!

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-
-    lenght = len(sys.argv) - 1
-    if lenght is 0:
-        print("0 arguments.")
-    elif lenght is 1:
-        print("1 argument:")
+    argv = sys.argv[1:]
+    argv_count = len(argv)
+    index = 1
+    if argv_count is 0:
+        print("{:d} arguments.".format(argv_count))
+    elif argv_count is 1:
+        print("{:d} argument:".format(argv_count))
+        print("{:d}: {:s}".format(index, sys.argv[1]))
     else:
-        print("{} arguments:".format(lenght))
-
-    for i in range(lenght):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        print("{:d} arguments:".format(argv_count))
+        while index <= argv_count:
+            print("{:d}: {:s}".format(index, sys.argv[index]))
+            index += 1
---
+++ 0x02-python-import_modules/copy2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    lenght = len(sys.argv) - 1
+    if lenght is 0:
+        print("0 arguments.")
+    elif lenght is 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(lenght))
+
+    for i in range(lenght):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
