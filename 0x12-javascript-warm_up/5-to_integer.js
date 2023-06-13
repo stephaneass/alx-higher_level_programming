08dff8de8b1168bd1e73c613e2013de0a3b770b1
Stephane ASSOCLE <mensste30@gmail.com>
2023-06-13 07:45:11 -0700

    script that prints My number:

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const arg1 = parseInt(process.argv[2]);
-console.log(isNaN(arg1) ? 'My number: ' + arg1 : 'Not a number');
+console.log(!isNaN(arg1) ? 'My number: ' + arg1 : 'Not a number');
