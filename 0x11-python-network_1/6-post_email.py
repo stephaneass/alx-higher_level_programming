d850b05c45b3e2b714b5cc66f44a18d7245ab62e
Stephane ASSOCLE <mensste30@gmail.com>
2023-07-04 12:39:43 -0700

    Error code

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     from requests import post
     from sys import argv
 
-    data = post(argv[1], params={'email': argv[2]})
-    print(data.text)
+    result = post(argv[1], data={'email': argv[2]})
+    print(result.text)
